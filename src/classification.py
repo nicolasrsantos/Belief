bb71db66ce5e0cfe544318dbae27113025fc7e1a
Nícolas Roque <nicolas.rsantos1@gmail.com>
2018-11-29 17:01:27 -0200

    New stuff

--- src/classification.py
+++ src/classification.py
@@ -12,38 +12,64 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics import confusion_matrix
-from sklearn.metrics import accuracy_score
-from sklearn.preprocessing import LabelEncoder
-from sklearn.metrics import f1_score
-from sklearn.naive_bayes import BernoulliNB, MultinomialNB
-from sklearn import metrics
-from sklearn.metrics import precision_score
-from sklearn.metrics import recall_score
+from sklearn.naive_bayes import BernoulliNB
+from sklearn.preprocessing import label_binarize
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.model_selection import cross_val_score, KFold, LeaveOneOut, StratifiedKFold
 
-csv_path = "/home/nicolas/Documents/Scripts/Rumour Belief/csv/"
-stopwords_filepath = "/home/nicolas/Documents/Scripts/Rumour Belief/stopwords.txt"
-
-df = pd.read_csv(csv_path + "submissions_with labels.csv")
-#df = utils.clean_corpus(df, stopwords_filepath)
-
-X = df.Submission
-y = df.Label
-
-tfidf = TfidfVectorizer(sublinear_tf = True, min_df = 3, norm='l2', encoding='latin-1', ngram_range=(1, 2), stop_words = 'english', lowercase='True')
-X = tfidf.fit_transform(X).toarray()
-
-labelencoder_y = LabelEncoder()
-y = labelencoder_y.fit_transform(y)
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
-
-classifier = SVC(kernel = 'linear', random_state = 0)
-classifier.fit(X_train, y_train)
-y_pred = classifier.predict(X_test)
-
-cm = confusion_matrix(y_test, y_pred)
-print(accuracy_score(y_test, y_pred))
-print(f1_score(y_test, y_pred, average = 'weighted'))
-print(precision_score(y_test, y_pred, average = 'weighted'))
-print(recall_score(y_test, y_pred, average = 'weighted'))
+def execute_OneVsRestSVM(X_train, y_train, X_test, y_test):
+    classifier = OneVsRestClassifier(SVC(kernel = 'linear', probability = True, random_state = 0))
+    classifier.fit(X_train, y_train)
+    y_score = classifier.predict_proba(X_test)
+    utils.compute_ROC(n_classes, y_test, y_score)
+    
+def execute_OneVsRestNB(X_train, y_train, X_test, y_test):
+    classifier = OneVsRestClassifier(BernoulliNB())
+    classifier.fit(X_train, y_train)
+    y_score = classifier.predict_proba(X_test)
+    utils.compute_ROC(n_classes, y_test, y_score)
+    
+def execute_SVM(X_train, y_train, X_test, y_test):
+    classifier = SVC(kernel = 'linear', random_state = 0)
+    classifier.fit(X_train, y_train)
+    y_pred = classifier.predict(X_test)
+    utils.compute_metrics(y_test, y_pred)
+    
+def execute_NB(X_train, y_train, X_test, y_test):
+    classifier = BernoulliNB()
+    classifier.fit(X_train, y_train)
+    y_pred = classifier.predict(X_test)
+    utils.compute_metrics(y_test, y_pred)
+    
+if __name__ == "__main__":    
+    csv_path = "/home/nicolas/Documents/Scripts/Rumour Belief/csv/"
+    stopwords_filepath = "/home/nicolas/Documents/Scripts/Rumour Belief/stopwords.txt"
+    
+    df = pd.read_csv(csv_path + "submissions_with labels.csv")
+    X = df.Submission
+    y = df.Label
+    tfidf = TfidfVectorizer(sublinear_tf = True, min_df = 3, norm='l2', encoding='latin-1', ngram_range=(1, 2), stop_words = 'english', lowercase='True')
+    X = tfidf.fit_transform(X).toarray()
+    
+    #y = label_binarize(y, classes=['Believe', 'Disbelieve', 'Neutral'])
+    #n_classes = y.shape[1]
+    
+    #X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+    #execute_SVM(X_train, y_train, X_test, y_test)
+    #execute_NB(X_train, y_train, X_test, y_test)
+    
+# =============================================================================
+#     loo = LeaveOneOut() 
+#     classifier = BernoulliNB()
+#     scores = cross_val_score(classifier, X, y, cv = loo)
+#     print(np.mean(scores))
+# =============================================================================
+    
+    kf = StratifiedKFold(n_splits = 10, shuffle = True)
+    classifier = BernoulliNB()
+    scores = cross_val_score(classifier, X, y, cv = kf)
+    avg_score = np.mean(scores)
+    print(avg_score)
+    
+    #execute_OneVsRestSVM(X_train, y_train, X_test, y_test)
+    #execute_OneVsRestNB(X_train, y_train, X_test, y_test)
--- src/utils.py
+++ src/utils.py
@@ -10,6 +10,16 @@ import re
 import nltk
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.metrics import f1_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from itertools import cycle
+from scipy import interp
+from sklearn.metrics import roc_curve, auc
+
 
 def tokenize(text):
     return nltk.word_tokenize(text)
@@ -48,3 +58,66 @@ def displayScores(vectorizer, tfidf_result):
     print(len(sorted_scores))
     for item in sorted_scores:
         print("{0:50} Score: {1}".format(item[0], item[1]))
+
+def compute_metrics(y_test, y_pred):
+    #print(confusion_matrix(y_test, y_pred))
+    print(accuracy_score(y_test, y_pred))
+    print(f1_score(y_test, y_pred, average = 'weighted'))
+    print(precision_score(y_test, y_pred, average = 'weighted'))
+    print(recall_score(y_test, y_pred, average = 'weighted'))
+    
+def compute_ROC(n_classes, y_test, y_score):
+    lw = 2
+    fpr = dict()
+    tpr = dict()
+    roc_auc = dict()
+    for i in range(n_classes):
+        fpr[i], tpr[i], _ = roc_curve(y_test[:, i], y_score[:, i])
+        roc_auc[i] = auc(fpr[i], tpr[i])
+    
+    fpr["micro"], tpr["micro"], _ = roc_curve(y_test.ravel(), y_score.ravel())
+    roc_auc["micro"] = auc(fpr["micro"], tpr["micro"])
+    
+    # First aggregate all false positive rates
+    all_fpr = np.unique(np.concatenate([fpr[i] for i in range(n_classes)]))
+    
+    # Then interpolate all ROC curves at this points
+    mean_tpr = np.zeros_like(all_fpr)
+    for i in range(n_classes):
+        mean_tpr += interp(all_fpr, fpr[i], tpr[i])
+    
+    # Finally average it and compute AUC
+    mean_tpr /= n_classes
+    
+    fpr["macro"] = all_fpr
+    tpr["macro"] = mean_tpr
+    roc_auc["macro"] = auc(fpr["macro"], tpr["macro"])
+    
+    # Plot all ROC curves
+    plt.figure()
+# =============================================================================
+#     plt.plot(fpr["micro"], tpr["micro"],
+#              label='micro-average ROC curve (area = {0:0.2f})'
+#                    ''.format(roc_auc["micro"]),
+#              color='deeppink', linestyle=':', linewidth=4)
+#     
+#     plt.plot(fpr["macro"], tpr["macro"],
+#              label='macro-average ROC curve (area = {0:0.2f})'
+#                    ''.format(roc_auc["macro"]),
+#              color='navy', linestyle=':', linewidth=4)
+# =============================================================================
+    
+    colors = cycle(['aqua', 'darkorange', 'cornflowerblue'])
+    for i, color in zip(range(n_classes), colors):
+        plt.plot(fpr[i], tpr[i], color = color, lw = lw,
+                 label = 'ROC curve of class {0} (area = {1:0.2f})'
+                 ''.format(i, roc_auc[i]))
+    
+    plt.plot([0, 1], [0, 1], 'k--', lw = lw)
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('ROC curve')
+    plt.legend(loc = "lower right")
+    plt.show()
